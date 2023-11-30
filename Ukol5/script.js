2550afecae362a3dd29366e9b8fe790fd3eb8780
Katarina Danielcakova <katadan@yahoo.com>
2023-11-30 12:54:15 +0100

    Ukol 5 splnen

---
+++ Ukol5/script.js
@@ -0,0 +1,22 @@
+const salary = (wage, hours, days) => {
+    
+    return wage * hours * days
+}
+
+const wage = 600
+const hours = 8
+const days = 20
+
+console.log(salary(wage, hours, days))
+
+const taxed = (castka, procento) => {
+    return castka - (castka * procento / 100)
+}
+
+const castka = salary(wage, hours, days)
+const procento = 15
+
+console.log(taxed(castka, procento))
+
+
+
